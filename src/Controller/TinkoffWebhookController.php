17b105308d6bc4c02f7cea641f105761df7df2bd
Ivan Gulin <doctor254452009@ya.ru>
2025-06-04 23:24:24 +0500

    refactor TinkoffWebhookController

--- src/Controller/TinkoffWebhookController.php
+++ src/Controller/TinkoffWebhookController.php
@@ -5,8 +5,8 @@ namespace App\Controller;
 use App\Domain\Order\Service\TinkoffWebhookService;
 use App\Domain\Order\TransferObject\Tinkoff\WebhookRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -17,20 +17,16 @@ final class TinkoffWebhookController extends AbstractController
     }
 
     #[Route('/order/success', name: 'order_success')]
-    public function success(Request $request): Response
+    public function success(#[MapRequestPayload] WebhookRequest $webhookRequest): Response
     {
-        $webhookRequest = $this->serializer->deserialize($request->getContent(), WebhookRequest::class, 'json');
-
         $this->tinkoffWebhookService->onSuccess($webhookRequest);
 
         return new Response('Ваш заказ успешно оформлен!');
     }
 
     #[Route('/order/failure', name: 'order_failure')]
-    public function failure(Request $request): Response
+    public function failure(#[MapRequestPayload] WebhookRequest $webhookRequest): Response
     {
-        $webhookRequest = $this->serializer->deserialize($request->getContent(), WebhookRequest::class, 'json');
-
         $this->tinkoffWebhookService->onFailure($webhookRequest);
 
         return new Response('Ошибка при оформлении заказа!');
