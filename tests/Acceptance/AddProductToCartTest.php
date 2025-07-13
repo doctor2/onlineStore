b9d55ee65c041b0eca01a8482fe947bed20f6845
Ivan Gulin <doctor254452009@ya.ru>
2025-07-13 18:24:01 +0500

    fix tests

--- tests/Acceptance/AddProductToCartTest.php
+++ tests/Acceptance/AddProductToCartTest.php
@@ -4,84 +4,112 @@ declare(strict_types=1);
 
 namespace Tests\Acceptance;
 
+use App\Factory\Helper\DefaultUserPasswordGenerator;
 use App\Factory\ProductFactory;
 use App\Factory\UserFactory;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Panther\PantherTestCaseTrait;
+use Zenstruck\Browser\PantherBrowser;
 use Zenstruck\Browser\Test\HasBrowser;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
 final class AddProductToCartTest extends KernelTestCase
 {
-    use HasBrowser, ResetDatabase, Factories;
+    use HasBrowser, ResetDatabase, Factories, PantherTestCaseTrait;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->pantherBrowser()->client()->restart();
+    }
 
     public function testAddTwoSameProductByAnonymousUser(): void
     {
-        $product = ProductFactory::createOne();
-
-        $this->browser()
-            ->visit('/cart')
-            ->assertSee('Пусто')
-            ->visit('/')
-            ->click(sprintf('form[action="/cart/add-product/%s"] [type="submit"]', $product->getId()))
-            ->assertSee('Товар добавлен в корзину!')
-            ->click(sprintf('form[action="/cart/add-product/%s"] [type="submit"]', $product->getId()))
-            ->assertSee('Товар добавлен в корзину!')
-            ->visit('/cart')
-            ->assertElementCount('.item-details', 1)
-            ->assertSeeIn('.item-name', $product->getName())
-            ->assertSeeIn('.item-price', $product->getPrice() . ' ₽')
-            ->assertSeeIn('#quantity-' . $product->getId(), '2')
-            ->assertSee(sprintf('Итого: %s ₽', $product->getPrice() * 2 ));
+        $this->addTwoSameProduct($this->pantherBrowser());
     }
 
     public function testAddTwoDifferentProductByAnonymousUser(): void
     {
-        $product = ProductFactory::createOne();
-        $product1 = ProductFactory::createOne();
-
-        $this->browser()
-            ->visit('/cart')
-            ->assertSee('Пусто')
-            ->visit('/')
-            ->click(sprintf('form[action="/cart/add-product/%s"] [type="submit"]', $product->getId()))
-            ->assertSee('Товар добавлен в корзину!')
-            ->click(sprintf('form[action="/cart/add-product/%s"] [type="submit"]', $product1->getId()))
-            ->assertSee('Товар добавлен в корзину!')
-            ->visit('/cart')
-            ->assertElementCount('.item-details', 2)
-            ->assertSeeIn('.item-name', $product->getName())
-            ->assertSeeIn('.item-price', $product->getPrice() . ' ₽')
-            ->assertSeeIn('#quantity-' . $product->getId(), '1')
-            ->assertSeeIn('.cart-items li:nth-child(2) .item-name', $product1->getName())
-            ->assertSeeIn('.cart-items li:nth-child(2) .item-price', $product1->getPrice() . ' ₽')
-            ->assertSeeIn('#quantity-' . $product1->getId(), '1')
-            ->assertSee(sprintf('Итого: %s ₽', $product->getPrice() + $product1->getPrice() ));
+        $this->addTwoDifferentProduct($this->pantherBrowser());
     }
 
     public function testAddTwoSameProductByCustomer(): void
     {
         $customer = UserFactory::new()->customer()->create();
 
-        $this->browser()
-            ->actingAs($customer)
+        $browser = $this->loginAs($customer->getEmail(), DefaultUserPasswordGenerator::DEFAULT_USER_PASSWORD)
             ->visit('/')
-            ->assertSee($customer->getUsername())
-        ;
+            ->assertSee('Здравствуйте, ' . $customer->getUsername());
 
-        $this->testAddTwoSameProductByAnonymousUser();
+        $this->addTwoSameProduct($browser);
     }
 
     public function testAddTwoDifferentProductByCustomer(): void
     {
         $customer = UserFactory::new()->customer()->create();
 
-        $this->browser()
-            ->actingAs($customer)
+        $browser = $this->loginAs($customer->getEmail(), DefaultUserPasswordGenerator::DEFAULT_USER_PASSWORD)
             ->visit('/')
-            ->assertSee($customer->getUsername())
+            ->assertSee('Здравствуйте, ' . $customer->getUsername())
         ;
 
-        $this->testAddTwoDifferentProductByAnonymousUser();
+        $this->addTwoDifferentProduct($browser);
+    }
+
+    public function addTwoSameProduct(?PantherBrowser $browser = null): void
+    {
+        $product = ProductFactory::createOne();
+
+        $browser
+            ->visit('/cart')
+            ->waitUntilVisible('#app-cart')
+            ->assertSee('Пусто')
+            ->visit('/')
+            ->click(sprintf('[data-product-id="%s"]', $product->getId()))
+            ->assertSee('Товар добавлен в корзину!')
+            ->click(sprintf('[data-product-id="%s"]', $product->getId()))
+            ->assertSee('Товар добавлен в корзину!')
+            ->visit('/cart')
+            ->waitUntilVisible('#app-cart')
+            ->assertElementCount('.item-details', 1)
+            ->assertSeeIn('.item-name', $product->getName())
+            ->assertSeeIn('#quantity-' . $product->getId(), '2')
+        ;
+    }
+
+    public function addTwoDifferentProduct(?PantherBrowser $browser = null): void
+    {
+        $product = ProductFactory::createOne();
+        $product1 = ProductFactory::createOne();
+
+        $browser
+            ->visit('/cart')
+            ->waitUntilVisible('#app-cart')
+            ->assertSee('Пусто')
+            ->visit('/')
+            ->click(sprintf('button[data-product-id="%s"]', $product->getId()))
+            ->assertSee('Товар добавлен в корзину!')
+            ->click(sprintf('button[data-product-id="%s"]', $product1->getId()))
+            ->assertSee('Товар добавлен в корзину!')
+            ->visit('/cart')
+            ->waitUntilVisible('#app-cart')
+            ->assertElementCount('.item-details', 2)
+            ->assertSeeIn('.item-name', $product->getName())
+            ->assertSeeIn('#quantity-' . $product->getId(), '1')
+            ->assertSeeIn('.cart-items li:nth-child(2) .item-name', $product1->getName())
+            ->assertSeeIn('#quantity-' . $product1->getId(), '1')
+        ;
+    }
+
+    protected function loginAs(string $email, string $password): PantherBrowser
+    {
+        return $this->pantherBrowser()
+            ->visit('/login')
+            ->fillField('username', $email)
+            ->fillField('password', $password)
+            ->click('Sign in')
+        ;
     }
 }
